c95d6776802454462721479cef3c0abd806310ca
throne.yash@gmail.com <throne.yash@gmail.com>
2021-06-23 21:56:23 -0600

    First Leetcode Problem

---
+++ Two_Sum.js
@@ -0,0 +1,66 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+// Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+const { PerformanceObserver, performance } = require('perf_hooks')
+
+let t0 = performance.now()
+
+let twoSum1 = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let k = i + 1; k < nums.length; k++) {
+      if (nums[i] + nums[k] === target) {
+        return [i, k]
+      }
+    }
+  }
+}
+
+console.log(twoSum1([1, 3, 15, 30, 45, 100, 3, 2, 7, 11, 15], 9))
+
+let t1 = performance.now()
+console.log(`It took the twoSum1 function ${t1 - t0} milliseconds`)
+
+t0 = performance.now()
+
+let twoSum2 = function (nums, target) {
+  let obj = {}
+  for (let i = 0; i < nums.length; i++) {
+    let currNum = nums[i]
+    let diff = target - currNum
+    if (typeof obj[diff] === 'number') {
+      return [obj[diff], i]
+    }
+    obj[currNum] = i
+  }
+}
+
+console.log(twoSum2([1, 3, 15, 30, 45, 100, 3, 2, 7, 11, 15], 9))
+
+t1 = performance.now()
+console.log(`It took the twoSum2 function ${t1 - t0} milliseconds`)
