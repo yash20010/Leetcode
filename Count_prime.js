a93545bdaa6b3265300c6d1c2ebc0c472aaa074f
throne.yash@gmail.com <throne.yash@gmail.com>
2021-08-18 19:07:41 -0600

    Adding more challenges

---
+++ Count_prime.js
@@ -0,0 +1,34 @@
+// Count the number of prime numbers less than a non-negative number, n.
+
+// Example 1:
+
+// Input: n = 10
+// Output: 4
+// Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+// Example 2:
+
+// Input: n = 0
+// Output: 0
+// Example 3:
+
+// Input: n = 1
+// Output: 0
+
+// Constraints:
+
+// 0 <= n <= 5 * 106
+
+const countPrimes = function (n) {
+  let seen = new Uint8Array(n),
+    ans = 0
+  for (let num = 2; num < n; num++) {
+    if (seen[num]) continue
+    ans++
+    for (let mult = num * num; mult < n; mult += num) {
+      seen[mult] = 1
+    }
+  }
+  return ans
+}
+
+// new Uint8Array() is faster to access and takes less space in memory than new Array()
