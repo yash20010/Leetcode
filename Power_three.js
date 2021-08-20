1dcd57455b96488d96fa6eb535760800630bcf55
throne.yash@gmail.com <throne.yash@gmail.com>
2021-08-19 19:08:22 -0600

    Adding more challenges

---
+++ Power_three.js
@@ -0,0 +1,33 @@
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+// Example 1:
+
+// Input: n = 27
+// Output: true
+// Example 2:
+
+// Input: n = 0
+// Output: false
+// Example 3:
+
+// Input: n = 9
+// Output: true
+// Example 4:
+
+// Input: n = 45
+// Output: false
+
+// Constraints:
+
+// -231 <= n <= 231 - 1
+
+// Follow up: Could you solve it without loops/recursion?
+
+var isPowerOfThree = function (n) {
+  return n > 0 && (Math.log(n) / Math.log(3)).toFixed(10) % 1 === 0
+  //   return n > 0 && 1162261467 % n === 0
+}
+
+console.log(isPowerOfThree(9))
